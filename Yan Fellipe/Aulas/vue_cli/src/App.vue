ea2aef0c77b7732270b0e379a70cb45de4016dfc
Yan Fellipe da Silva Neri <yan.neri@dinfor.uniceub>
2026-03-26 21:07:45 -0300

    Diretivas

--- Yan Fellipe/Aulas/vue_cli/src/App.vue
+++ Yan Fellipe/Aulas/vue_cli/src/App.vue
@@ -1,20 +1,40 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Meu App Vue</h1>
+    <p class="subtitulo">Meu subtítulo</p>
     <br />
     <home-component/>
-    <hr>
-    <home-component/>
+    <br/>
+    <usuario-component/>
+    <br>
+    <estilo-component/>
   </div>
 </template>
 
 <script>
 import HomeComponent from './components/HomeComponent.vue';
+import UsuarioComponent from './components/UsuarioComponent.vue';  
+import EstiloComponent from './components/EstiloComponent.vue';
 
 export default {
   name: 'App',
   components: {
     HomeComponent,
+    UsuarioComponent,
+    EstiloComponent,
+  },
+  created() {
+    console.log("created");
+  },
+  mounted() {
+    console.log("mounted")
+  },
+  updated() {
+    console.log("uptaded")
+  },
+  errorCaptured(){
+    console.log("errorCaptured")
   },
 };
 </script>
@@ -27,5 +47,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1{
+  color: darkmagenta;
+  text-align: center;
+}
+
+.subtitulo{
+  color: darkseagreen;
+  font-family:"Gill Sans";
+  text-align: center;
 }
 </style>
